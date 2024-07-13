d91270f158bb6a0fedc7a5ef5d6edab1a7998329
arvindkumar20 <arvindkumar8960905167@gmail.com>
2024-07-13 23:59:18 +0530

    add tailwind and change tailwind.config.js darkMode based on class

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
 
-import Login from "./components/Login"
+import { useEffect, useState } from 'react'
 
-import Profile from "./components/Profile"
-import Providers from "./context/Providers"
-export default function App() {
+import { ThemeProvider } from './context/theme'
+import ThemeBtn from './components/ThemesBtn'
+import Card from './components/Card'
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
 
   return (
-    <div>
-      <Providers>
-  <Login />
-  <Profile/>
-  </Providers>
-    </div>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
   )
 }
+
+export default App
